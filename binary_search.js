424ea42853412525a2d1281c9b71bbf54cf36aff
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2019-02-03 17:36:34 +0300

    Create binary_search.js

---
+++ binary_search.js
@@ -0,0 +1,16 @@
+function binary_search(arr,value) {
+var low  = 0;
+var high = Arr.length -1;
+var mid;      
+        while (low <= high){
+            mid = Math.floor((low+high)/2);     
+            if(Arr[mid]===value){
+            	return mid
+            }else if (Arr[mid]<value){
+            	low = mid+1;
+            }else{
+            	high = mid-1;
+            }          
+        }
+        return -1 ;
+}
