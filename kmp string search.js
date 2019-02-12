c28697b3bbe3d61f9d105da8c6dc1d14748f5d42
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2019-02-12 21:25:10 +0300

    Create kmp string search.js

---
+++ kmp string search.js
@@ -0,0 +1,51 @@
+function prefix_function(word) {
+  let patternTable = [0];
+  let prefixIndex = 0;
+  let suffixIndex = 1;
+
+  while (suffixIndex < word.length) {
+    if (word[prefixIndex] === word[suffixIndex]) {
+      patternTable[suffixIndex] = prefixIndex + 1;
+      suffixIndex += 1;
+      prefixIndex += 1;
+    } else if (prefixIndex === 0) {
+      patternTable[suffixIndex] = 0;
+      suffixIndex += 1;
+    } else {
+      prefixIndex = patternTable[prefixIndex - 1];
+    }
+  }
+
+  return patternTable;
+}
+
+ function kmp(text, word) {
+  if (word.length === 0) {
+    return 0;
+  } 
+  let matches=[]
+  let textIndex = 0;
+  let wordIndex = 0;
+  const patternTable = prefix_function(word);
+
+  while (textIndex < text.length) {
+    if (text[textIndex] === word[wordIndex]) {
+      if (wordIndex === word.length - 1) {
+        matches.push((textIndex - word.length) + 1)
+      }
+      wordIndex += 1;
+      textIndex += 1;
+    } else if (wordIndex > 0) {
+      wordIndex = patternTable[wordIndex - 1];
+    } else {
+      wordIndex = 0;
+      textIndex += 1;
+    }
+  }
+  if(matches.length===0){
+    return -1;
+  }else{
+    return matches
+  }
+  
+}
