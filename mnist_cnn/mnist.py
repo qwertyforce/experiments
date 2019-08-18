65601fe7c6ef499a6b3a67ee6d088285d19b580d
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2019-08-18 14:06:00 +0300

    mnist tensorboard

--- mnist_cnn/mnist.py
+++ mnist_cnn/mnist.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
+from datetime import datetime
+logdir = "logs" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape((60000, 28, 28, 1))
@@ -20,5 +23,5 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train,batch_size=32, epochs=10)
+model.fit(x_train, y_train,batch_size=32,callbacks=[tensorboard_callback],validation_data=(x_test, y_test), epochs=5)
 model.evaluate(x_test, y_test)
--- mnist_simple/mnist.py
+++ mnist_simple/mnist.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
+from datetime import datetime
+logdir = "logs" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
@@ -14,5 +17,5 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train,batch_size=32, epochs=10)
+model.fit(x_train, y_train,batch_size=32, callbacks=[tensorboard_callback],epochs=10,validation_data=(x_test, y_test))
 model.evaluate(x_test, y_test)
