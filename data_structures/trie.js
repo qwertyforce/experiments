75a6560c282d8b9d1fb1cda18ce5f43af7a3d5ee
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-06-03 10:52:47 +0300

    Create trie.js

---
+++ data_structures/trie.js
@@ -0,0 +1,54 @@
+class Node {
+    constructor(value) {
+        this.value=value
+        this.children={}
+        this.is_complete_word = false
+    }
+    
+}
+
+class Trie{
+    constructor(){
+        this.root=new Node()
+        this.words=[]
+    }
+   
+    insert(word){
+        let node=this.root
+        for (const chr of word) {
+            if(!node.children[chr]){
+                node.children[chr]=new Node(chr)
+            }
+            node=node.children[chr]
+        }
+        node.is_complete_word=true
+    }
+    
+
+    find_prefix(prefix){
+        this.words=[]
+        let node=this.root
+        for (const chr of prefix) {
+            if(!node.children[chr]){
+                console.log("Not found")
+                return false
+            }
+            node=node.children[chr]
+        }
+
+        // const words=[]
+        this._find_prefix(node,prefix)
+        return this.words
+    }
+
+    _find_prefix(node, curr_word) {
+        console.log(node)
+        if (node.is_complete_word) {
+            this.words.push(curr_word)
+        }
+        for (let child in node.children) {
+            this._find_prefix(node.children[child], curr_word + node.children[child].value)
+        }
+    }
+
+}
