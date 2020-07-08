68e28155561639f614a718b57cb3a89efd0bc9e2
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-07-08 20:37:52 +0300

    Create 3sum.js

---
+++ 3sum.js
@@ -0,0 +1,24 @@
+function three_sum (nums) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+    
+    for(let i = 0; i < nums.length; i++) {
+        let low = i+1;
+        let high = nums.length-1;
+        let sum = 0;
+        let current_number=nums[i]
+        while(low < high) {
+            sum =  current_number + nums[low] + nums[high]; 
+            if(sum === 0) {
+                result.push([nums[i], nums[low], nums[high]]);
+                // while(nums[low+1] === nums[low]) low++;
+                // while(nums[high-1] === nums[high]) high--;
+                low++;
+                high--;
+            } else if(sum < 0) low++;
+            else high--;
+        }
+        while(nums[i+1] === nums[i]) i++;
+    }
+    return result;    
+};
