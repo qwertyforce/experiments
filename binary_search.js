7835c5aa0e67679adb1501ddac15a9c597a6fe06
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-07-21 17:08:12 +0300

    Update binary_search.js

--- binary_search.js
+++ binary_search.js
@@ -1,12 +1,12 @@
 function binary_search(arr, value) {
     var low = 0;
-    var high = Arr.length - 1;
+    var high = arr.length - 1;
     var mid;
     while (low <= high) {
         mid = Math.floor((low + high) / 2);
-        if (Arr[mid] === value) {
+        if (arr[mid] === value) {
             return mid
-        } else if (Arr[mid] < value) {
+        } else if (arr[mid] < value) {
             low = mid + 1;
         } else {
             high = mid - 1;
