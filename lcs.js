5f53e3d34548e0cfa55e44bda6529c577d9f6076
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-08-04 12:35:57 +0300

    Update lcs.js

--- lcs.js
+++ lcs.js
@@ -19,3 +19,26 @@ function lcs_rec(text1, text2) {
     }
     return lcs(text1.length,text2.length)
 };
+
+function lcs_dp(text1, text2) {
+    let dp=[]
+    for(let i = 0; i <= text1.length; i++) {
+        dp.push(new Array(text2.length + 1).fill(0));
+    }
+    for(let i=0;i<dp[0].length;i++){
+        dp[0][i]=0
+    }
+    for(let i=0;i<dp.length;i++){
+        dp[i][0]=0
+    }
+    for(let i=1;i<dp.length;i++){
+        for(let j=1;j<dp[0].length;j++){
+            if(text1[i-1]===text2[j-1]){
+                dp[i][j]=dp[i-1][j-1]+1
+            }else{
+                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1])
+            }
+        }
+    }
+    return dp[dp.length-1][dp[0].length-1]
+};
