c309342e243aeb1f6b52e35006f53f8f4ad7a978
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2021-03-08 17:27:32 +0300

    Update batch_get_data.ts

--- mongo_db_snippets/batch_get_data.ts
+++ mongo_db_snippets/batch_get_data.ts
@@ -1,5 +1,5 @@
 async function batch_get_data(skip:number,limit:number){
     const collection = client.db(db_main).collection("collection_name");
-    const getDataAsArray = collection.find().skip(skip).limit(limit).project({_id:0}).toArray()
+    const data = collection.find().skip(skip).limit(limit).project({_id:0}).toArray()
     return data
 }
