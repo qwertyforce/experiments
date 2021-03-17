be784b13fa227400d487ed5d5cd07d8e18dcb193
qwertyforce <qwertyforce@protonmail.com>
2021-03-17 22:14:47 +0300

    Create random_point_inside_circle.js

---
+++ leetcode/random_point_inside_circle.js
@@ -0,0 +1,9 @@
+const angle = Math.random() * 2 * Math.PI;
+const radius = 2
+const center_x=2
+const center_y=2
+const hypotenuse Math.random() * radius
+const adjacent = Math.cos(angle) * hypotenuse
+const opposite = Math.sin(angle) * hypotenuse
+const point_x = center_x + adjacent
+const point_y = center_y + opposite
